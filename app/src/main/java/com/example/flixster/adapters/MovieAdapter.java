754f7b9c93852cb23fe7c0ce6a78d79c4eab2089
SadockTheodor <Sadockt9@gmail.com>
2020-03-12 17:43:26 -0400

    my second commit.

--- app/src/main/java/com/example/flixster/adapters/MovieAdapter.java
+++ app/src/main/java/com/example/flixster/adapters/MovieAdapter.java
@@ -1,6 +1,7 @@
 package com.example.flixster.adapters;
 
 import android.content.Context;
+import android.content.res.Configuration;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -69,7 +70,17 @@ public class MovieAdapter extends  RecyclerView.Adapter<MovieAdapter.ViewHolder>
         public void bind(movie movie) {
             tvTitle.setText(movie.getTitle());
             tvOverview.setText(movie.getOverView());
-            Glide.with(context).load(movie.getPosterPath()).into(ivPoster);
+            String imageUrl;
+            //if phone is on landscape
+            if (context.getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE){
+                //then imageUrl = back drop image
+                imageUrl = movie.getBackdrop_path();
+            }
+           else {
+                // else imageUrl = poster image
+                imageUrl = movie.getPosterPath();
+            }
+            Glide.with(context).load(imageUrl).into(ivPoster);
 
         }
     }
--- app/src/main/java/com/example/flixster/models/movie.java
+++ app/src/main/java/com/example/flixster/models/movie.java
@@ -8,12 +8,14 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class movie {
+    String backdrop_path;
     String posterPath;
     String title;
     String overView;
 
     public movie(JSONObject jsonObject) throws JSONException {
         posterPath = jsonObject.getString("poster_path");
+        backdrop_path = jsonObject.getString("backdrop_path");
         title = jsonObject.getString("title");
         overView = jsonObject.getString("overview");
 
@@ -28,6 +30,9 @@ public class movie {
 
     public String getPosterPath() {
         return String.format("https://image.tmdb.org/t/p/w342/%s", posterPath);
+    }
+    public  String getBackdrop_path(){
+        return String.format("https://image.tmdb.org/t/p/w342/%s", backdrop_path);
     }
 
     public String getTitle() {
